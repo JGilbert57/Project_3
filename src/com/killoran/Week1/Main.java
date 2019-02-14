d3a823d5e8a5de9b1712b430b30e6463273ce8a5
111618 <johnnygilbert55@gmail.com>
2019-02-14 12:41:32 -0500

    v2

--- src/com/killoran/Week1/Main.java
+++ src/com/killoran/Week1/Main.java
@@ -1,47 +1,60 @@
 package com.killoran.Week1;
-
 import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
+        Map<String, String> Address = new TreeMap<>();
+        option(Address);
+    }
+
+    public static void option(Map<String, String> Address) {
         Scanner Input = new Scanner(System.in);
-        Scanner dInput = new Scanner(System.in);
+        System.out.println("Enter 1 to add a person and there email");
+        System.out.println("Enter 2 to search for a person");
+        int choice = Input.nextInt();
+        if (choice == 1) {
+
+            add(Address);
+        }
+        if (choice == 2) {
+            search(Address);
+        }
+    }
+
+    public static void add(Map<String, String> Address) {
+        Scanner Input = new Scanner(System.in);
         String decision = "";
-        Map<String, Double> cities = new TreeMap<>();
         while (!decision.equals("END")) {
-            System.out.println("What city do you plan on visiting");
-            String City = dInput.nextLine();
-            System.out.println("What is your high temp for next 5 days in " + City);
-            System.out.println("Enter your temp for day 1");
-            double d1 = userInput.nextInt();
-            System.out.println("Enter your temp for day 2");
-            double d2 = userInput.nextInt();
-            System.out.println("Enter your temp for day 3");
-            double d3 = userInput.nextInt();
-            System.out.println("Enter your temp for day 4");
-            double d4 = userInput.nextInt();
-            System.out.println("Enter your temp for day 5");
-            double d5 = userInput.nextInt();
-            double avg = (calc(d1, d2, d3, d4, d5));
-            cities.put(City, avg);
-            System.out.println("Input END to stop adding cities else type anything you want");
+            System.out.println("Enter a Name");
+            String name = Input.nextLine();
+            System.out.println("What is your Email");
+            String email = Input.nextLine();
+            Address.put(name, email);
+            System.out.println("To End process type END");
             decision = Input.nextLine();
         }
-        display(cities);
-    }
-
-    static double calc(double d1, double d2, double d3, double d4, double d5) {
-        double avgHigh = (d1 + d2 + d3 + d4 + d5) / 5;
-        return avgHigh;
+        option(Address);
 
     }
 
-    public static void display(Map<String, Double> cities) {
-        for (String city : cities.keySet()) {
-            System.out.println("The Average High temperature in " + city + " is " + cities.get(city));
+    public static void search(Map<String, String> Address) {
+        Scanner Input = new Scanner(System.in);
+        String decision = "";
+        while (!decision.equals("END")) {
+            System.out.println("What is the name you would like to search");
+            String choice = Input.nextLine();
+            for (String name : Address.keySet()) {
+                if (name.equals(choice)) {
+                    Book person = new Book(name, Address.get(name));
+                    System.out.println(person.getEmail());
+                }
+                else {
+                    continue;
+                }
+            }
+            System.out.println("Type END to finish searching for Email addresses");
+            decision = Input.nextLine();
         }
+        option(Address);
     }
 }
-
